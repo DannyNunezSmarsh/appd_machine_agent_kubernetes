b34fb116558e74fd4124d7e6c6c399c582e3f50c
Mark Jeffery <mark@jeffery.com>
2016-10-21 14:38:44 +0100

    Added Dockerfile and fixed startup script

--- start_machine_agent_kubernetes.sh
+++ start_machine_agent_kubernetes.sh
@@ -1,2 +1,3 @@
 KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
-curl -sS --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  -H "Authorization: Bearer $KUBE_TOKEN"       https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME | grep nodeName | awk -F\" '{print $4}'
+UNIQUE_HOST_ID=$(curl -sS --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  -H "Authorization: Bearer $KUBE_TOKEN"       https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME | grep nodeName | awk -F\" '{print $4}')
+/bin/machine-agent -Dappdynamics.agent.uniqueHostId=$UNIQUE_HOST_ID
