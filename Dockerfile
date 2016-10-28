f840f173c9c7a710ff077ba39cef232d3557f994
Mark Jeffery <mark@jeffery.com>
2016-10-28 21:31:34 +0100

    Updates

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM openjdk:7
 ADD start_machine_agent_kubernetes.sh .
-CMD ["/start_machine_agent_kubernetes.sh"]
+COPY /appd_machine_agent/ /appd_machine_agent
+CMD ["./start_machine_agent_kubernetes.sh"]
--- start_machine_agent_kubernetes.sh
+++ start_machine_agent_kubernetes.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
 KUBE_TOKEN=$(</var/run/secrets/kubernetes.io/serviceaccount/token)
-UNIQUE_HOST_ID=$(curl -sS --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  -H "Authorization: Bearer $KUBE_TOKEN"       https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME | grep nodeName | awk -F\" '{print $4}')
-/home/mark_jeffery/appd_machine_agent_kubernetes/appd/machine-agent/bin/machine-agent -Dappdynamics.agent.uniqueHostId=$UNIQUE_HOST_ID
+echo $KUBE_TOKEN
+APPDYNAMICS_AGENT_UNIQUE_HOST_ID=$(curl -sS --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt  -H "Authorization: Bearer $KUBE_TOKEN"       https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_PORT_443_TCP_PORT/api/v1/namespaces/default/pods/$HOSTNAME | grep nodeName | awk -F\" '{print $4}')
+echo "Unique Host ID should be: $APPDYNAMICS_AGENT_UNIQUE_HOST_ID"
+echo "Hostname is $HOSTNAME"
+cd /appd_machine_agent
+java -Dappdynamics.agent.uniqueHostId=$APPDYNAMICS_AGENT_UNIQUE_HOST_ID -jar machineagent.jar
